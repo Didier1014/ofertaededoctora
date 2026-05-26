a94a4d020dc1d610cf5bf5b3e73fd05cba2c38ad
Didier1014 <matuevampiro123@gmail.com>
2026-05-26 09:40:31 -0700

    Replace Wistia player with Panda Video VSL iframe

--- src/components/WistiaVideoPlayer.tsx
+++ src/components/WistiaVideoPlayer.tsx
@@ -1,49 +1,15 @@
-import { useEffect, useRef } from "react"
-
 export function WistiaVideoPlayer() {
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const script = document.createElement("script")
-    script.src = "https://fast.wistia.com/player.js"
-    script.async = true
-    document.head.appendChild(script)
-
-    const embedScript = document.createElement("script")
-    embedScript.src = "https://fast.wistia.com/embed/g1lno4chi4.js"
-    embedScript.async = true
-    containerRef.current?.appendChild(embedScript)
-
-    return () => {
-      document.head.removeChild(script)
-    }
-  }, [])
-
   return (
-    <div className="mx-auto mb-4" style={{ maxWidth: 400 }}>
-      <div
-        ref={containerRef}
-        className="wistia_responsive_padding"
-        style={{ padding: "56.25% 0 0 0", position: "relative" }}
-      >
-        <div
-          className="wistia_responsive_wrapper"
-          style={{
-            height: "100%",
-            left: 0,
-            position: "absolute",
-            top: 0,
-            width: "100%",
-          }}
-        >
-          <div
-            className="wistia_embed wistia_async_g1lno4chi4 seo=true"
-            style={{ height: "100%", width: "100%" }}
-          >
-            &nbsp;
-          </div>
-        </div>
-      </div>
+    <div className="mx-auto mb-4" style={{ maxWidth: 720 }}>
+      <iframe
+        id="panda-05d51158-66d9-48c9-b2b9-2e86df0631a9"
+        src="https://player-vz-f9150b0d-544.tv.pandavideo.com.br/embed/?v=05d51158-66d9-48c9-b2b9-2e86df0631a9"
+        style={{ border: "none", width: "100%", height: "auto", aspectRatio: "720 / 360" }}
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture"
+        allowFullScreen
+        width="720"
+        height="360"
+      />
     </div>
   )
 }
