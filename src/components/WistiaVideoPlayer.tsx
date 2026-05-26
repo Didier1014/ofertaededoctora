bc2595eb31617c71d520321f241671ae77612736
Didier1014 <matuevampiro123@gmail.com>
2026-05-26 10:33:48 -0700

    Fix video aspect ratio to 16:9 using aspect-video

--- src/components/WistiaVideoPlayer.tsx
+++ src/components/WistiaVideoPlayer.tsx
@@ -1,14 +1,13 @@
 export function WistiaVideoPlayer() {
   return (
-    <div className="mx-auto mb-4" style={{ maxWidth: 720 }}>
+    <div className="mx-auto mb-4 aspect-video" style={{ maxWidth: 720 }}>
       <iframe
         id="panda-05d51158-66d9-48c9-b2b9-2e86df0631a9"
         src="https://player-vz-f9150b0d-544.tv.pandavideo.com.br/embed/?v=05d51158-66d9-48c9-b2b9-2e86df0631a9"
-        style={{ border: "none", width: "100%", height: "auto", aspectRatio: "720 / 360" }}
+        className="w-full h-full"
+        style={{ border: "none" }}
         allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture"
         allowFullScreen
-        width="720"
-        height="360"
       />
     </div>
   )
